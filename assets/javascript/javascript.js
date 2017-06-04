26a8ffa49b64ae55a0e27b21e7ca5695fabfb655
ArbutusNate <arbutusnate@gmail.com>
2017-06-04 11:19:30 -0700

    linking everything (firebase too)

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,10 +2,8 @@
   var fbconfig = {
     apiKey: "AIzaSyA8w-lQipqm-Ccbu7OB6w10TSL0XuWl9os",
     authDomain: "mrps-bf4df.firebaseapp.com",
-    databaseURL: "https://MRPS.firebaseio.com",
+    databaseURL: "https://mrps-bf4df.firebaseio.com",
     storageBucket: "mrps-bf4df.appspot.com"
   };
   firebase.initializeApp(fbconfig);
-
-  // Get a reference to the database service
   var database = firebase.database();
