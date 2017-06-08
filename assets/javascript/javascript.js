cd2984c59ea3c935664806ddf0be9dacdf5e8d6f
ArbutusNate <arbutusnate@gmail.com>
2017-06-08 16:31:25 -0700

    new stuff for styling

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -5,6 +5,7 @@
   var myChoice = ''
   var tempp1 = ''
   var tempp2 = ''
+
 // Firebase Setup
   var fbconfig = {
     apiKey: 'AIzaSyA8w-lQipqm-Ccbu7OB6w10TSL0XuWl9os',
@@ -14,6 +15,7 @@
   };
   firebase.initializeApp(fbconfig);
   var database = firebase.database();
+
 // Functions
   function postCompareReset(){
     myChoice = ''
@@ -27,8 +29,9 @@
       '...'
      )
      myChoice = ''
-     $('.bodyimg').hide()    
+     $('.bodyimg').hide()  
   }
+
   function p2Wins(){
     if(myChoice === tempp2){
       wins++
@@ -38,23 +41,34 @@
         choice: '...',
         wins: wins
       })
-      postCompareReset()
     }
+    postCompareReset()
     console.log('P2 wins.')
+    database.ref('players/player2').get(function(snapshot){
+      var winUpdate = snapshot.wins
+      $('#player2score').text(winUpdate)
+    })
+    $('.choicebtn').attr('disabled', false)
   }
+
   function p1Wins(){
     if(myChoice === tempp1){
       wins++
-      $('#player1score').text(wins)
       database.ref('players/player1').set({
         name:newplayer,
         choice: '...',
-        wins:wins
+        wins: wins
       })
-      postCompareReset()
     }
+    postCompareReset()
+    database.ref('players/player1').get(function(snapshot){
+      var winUpdate = snapshot.wins
+      $('#player1score').text(winUpdate)
+    })
     console.log('P1 wins.')
+    $('.choicebtn').attr('disabled', false)
   }
+
   function comparer(){
     database.ref('players/player1/choice').once('value').then(function(snapshot1){
       tempp1 = snapshot1.val()
@@ -91,36 +105,54 @@
       })
     })
   }
-// Listeners
-  // Listen for disconnect
-    var ref = database.ref('players/player' + playerNum)
-    ref
-      .onDisconnect()
-      .remove()
 
+  function playerStart(x){
+    $('#p' + x + 'box').addClass('active')
+    database.ref('players/player' + x).set({
+      name: newplayer,
+      choice: '...',
+      wins: 0
+    })
+      $('.phead' + x).text(newplayer)
+      database.ref('gameplay').set({
+        picked: 0
+      })
+      var ref = database.ref('players/player' + x)
+      ref.onDisconnect().remove()
+  }
+
+// Listeners
   // Listen for new players
     var player1Check = database.ref('players/player1')
     player1Check.on('value', function(snapshot){
       if(snapshot.exists() === false){
-        //do nothing
+        $('.phead1')
+          .text('Click to Join')
+          .addClass('joingame')
+        $('#player1score').text('0')
       } else {
         var tempPlayerCheck = snapshot.val()
         $('.phead1').text(tempPlayerCheck.name)
       }
     })
+
     var player2Check = database.ref('players/player2')
     player2Check.on('value', function(snapshot){
       if(snapshot.exists() === false){
-        //do nothing
+        $('.phead2').text('Click to Join')
       } else {
         var tempPlayerCheck = snapshot.val()
-        $('.phead2').text(tempPlayerCheck.name)
+        $('.phead2')
+            .text(tempPlayerCheck.name)
+            .addClass('joingame')
+        $('#player2score').text('0')
       }
     })
 
   // Listen for both choices
     var choiceCheck = database.ref('gameplay/picked')
     choiceCheck.on('value', function(snapshot){
+
       // When 1 is picked
         if(snapshot.val() === 1){
           if(playerNum === 1 && myChoice === ''){
@@ -136,13 +168,15 @@
             $('.waiting2').show()
           }
         }
+
       // When 2 are picked - Do all the logic
         if(snapshot.val() === 2){
-          setTimeout(comparer, 4000)
+          setTimeout(comparer, 2000)
         } else {
           //do nothing
         }
     })
+
 // Event Handlers
 
   // Join Game Click
@@ -154,35 +188,27 @@
 
       // Set Variables
         newplayer = ($('#addname').val().trim())
-        var p1joined = false
-        var p2joined = false
 
       // Firebase Calls
       database.ref('players/player1').once('value').then(function(snapshot){
         var checkplayer = snapshot.val()
         if(checkplayer === null){
-          database.ref('players/player1').set({
-            name: newplayer,
-            choice: '...',
-            wins: 0
-          })
-          $('.phead1').text(newplayer)
           playerNum = 1
+          playerStart(playerNum)
         } else {
           database.ref('players/player2').once('value').then(function(snapshot){
             var checkplayer2 = snapshot.val()
             if(checkplayer2 === null){
-              database.ref('players/player2').set({
-                name: newplayer,
-                choice: '...',
-                wins: 0
-              })
-              $('.phead2').text(newplayer)
               playerNum = 2
+              playerStart(playerNum)
             } else{
-              database.ref('observers').set({
+              // Observers
+              playerNum = 3
+              var observerRef = database.ref('observers/' + newplayer)
+              observerRef.set({
                 name: newplayer
               })
+              observerRef.onDisconnect().remove()
             }
           })
         }
@@ -217,25 +243,23 @@
       })
     })
 
-        
-
-
-// Listener Example
-  // var playerChange = database.ref('variables/playercount');
-  // playerChange.on('value', function(snapshot) {
-  //   playercount = snapshot.val();
-  //   console.log(snapshot.val());
-  // });
-// Retrieve data example
-  // var starCountRef = database.ref('posts/' + postId + '/starCount');
-  // starCountRef.on('value', function(snapshot) {
-  //   updateStarCount(postElement, snapshot.val());
-  // });
-// Write data example
-  // function writeUserData(userId, name, email, imageUrl) {
-  //   database.ref('users/' + userId).set({
-  //     username: name,
-  //     email: email,
-  //     profile_picture : imageUrl
-  //   });
-  // }
+// Examples
+  // Listener Example
+    // var playerChange = database.ref('variables/playercount');
+    // playerChange.on('value', function(snapshot) {
+    //   playercount = snapshot.val();
+    //   console.log(snapshot.val());
+    // });
+  // Retrieve data example
+    // var starCountRef = database.ref('posts/' + postId + '/starCount');
+    // starCountRef.on('value', function(snapshot) {
+    //   updateStarCount(postElement, snapshot.val());
+    // });
+  // Write data example
+    // function writeUserData(userId, name, email, imageUrl) {
+    //   database.ref('users/' + userId).set({
+    //     username: name,
+    //     email: email,
+    //     profile_picture : imageUrl
+    //   });
+    // }
